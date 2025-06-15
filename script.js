fc87964261114b1637b7305f2b7d2ad28320cf04
Rhoda Ugboro <rhodaugboro@gmail.com>
2025-06-15 18:06:46 +0100

    Added changes

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
         feedbackDiv.style.display = "block";
 
         if(isValid){
-            feedbackDiv.textContent = "Resgisteration successful!";
+            feedbackDiv.textContent = "Registration successful!";
             feedbackDiv.style.color = "#28a745";
         }
         else{
